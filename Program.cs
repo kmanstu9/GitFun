d01fe4045f9cb2c9cd78df173bd40fd8e78e1d0c
kmanstu9 <kmanstu9@byu.edu>
2025-01-28 14:54:29 -0700

    this is giong to eb a mistake

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine('hello world')
+Console.WriteLine('hello world');
+
+Console.WriteLine("this is gonna be a mistake");
