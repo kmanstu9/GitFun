2521018efe98bc61916ad51dbcbd40351d87b03d
kmanstu9 <kmanstu9@byu.edu>
2025-01-28 14:55:07 -0700

    Revert "this is giong to eb a mistake"
    This reverts commit d01fe4045f9cb2c9cd78df173bd40fd8e78e1d0c.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine('hello world');
-
-Console.WriteLine("this is gonna be a mistake");
+Console.WriteLine('hello world')
